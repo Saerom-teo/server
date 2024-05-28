46f38ed272f54fc571c6b7985302c7339af1d249
JhyunJ1 <jhyunj1005@gmail.com>
2024-05-28 12:41:32 +0900

    [Feat] : update create feature in EnvDataController

--- src/main/java/com/saeromteo/app/EnvDataController.java
+++ src/main/java/com/saeromteo/app/EnvDataController.java
@@ -1,5 +1,33 @@
 package com.saeromteo.app;
 
-public class EnvDataController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.saeromteo.app.dto.EnvironmentDataDto.Request;
+import com.saeromteo.app.dto.EnvironmentDataDto.Response;;
+import com.saeromteo.app.service.EnvDataService;
+
+@RestController
+@RequestMapping("/envdata/api/*")
+public class EnvDataController {
+	
+	@Autowired
+	EnvDataService envService;
+	
+	// Create
+	@PostMapping(value="/create", consumes="application/json", produces="text/plain;charset=UTF-8")
+	public int create(@RequestBody Request envDto) {
+		int result = envService.createEnvData(envDto);
+		return result;
+	}
+	
+	// Read
+	
+	// Update
+	
+	// Delete
+	
 }
