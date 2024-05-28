3aaf076331849e0967ff48411305b153e20aced2
kimsh142536 <kimsh142536@gmail.com>
2024-05-28 11:46:43 +0900

    [Setting] servlet sqlConfig path추가 sqlconfig 만듦

---
+++ src/main/java/com/saeromteo/app/TestDTO.java
@@ -0,0 +1,13 @@
+package com.saeromteo.app;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TestDTO {
+	private int idtest;
+	private String test;
+}
--- src/main/java/com/saeromteo/app/service/NumberService.java
+++ src/main/java/com/saeromteo/app/service/NumberService.java
@@ -3,6 +3,7 @@ package com.saeromteo.app.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.saeromteo.app.TestDTO;
 import com.saeromteo.app.dao.TestDao;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public class NumberService {
     @Autowired
     private TestDao numberRepository;
 
-    public List<Integer> getAllNumbers() {
+    public List<TestDTO> getAllNumbers() {
         return numberRepository.test();
     }
 }
