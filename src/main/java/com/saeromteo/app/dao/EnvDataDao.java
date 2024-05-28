32233b5093b0a0a1b6dd89d7b381863d5f57d732
JhyunJ1 <jhyunj1005@gmail.com>
2024-05-28 12:26:38 +0900

    [Rename] : fix code convention

--- src/main/java/com/saeromteo/app/dao/EnvDataDao.java
+++ src/main/java/com/saeromteo/app/dao/EnvDataDao.java
@@ -11,46 +11,45 @@ import com.saeromteo.app.dto.EnvironmentDataDto.Response;
 
 @Repository
 public class EnvDataDao {
-	
+
 	@Autowired
 	SqlSession sqlSession;
 
 	String namespace = "com.saerom.envdata.";
 
 	public int createEnvData(Request envDto) {
-		int result = sqlSession.insert(namespace+"createEnvData", envDto);
+		int result = sqlSession.insert(namespace + "createEnvData", envDto);
 		return result;
 	}
 
 	public List<Response> readAll() {
-		List<Response> envDataList = sqlSession.selectList(namespace+"readAll");
+		List<Response> envDataList = sqlSession.selectList(namespace + "readAll");
 		return envDataList;
 	}
 
 	public Response readDetail(int env_id) {
-		Response envData = sqlSession.selectOne(namespace+"readDetail", env_id);
+		Response envData = sqlSession.selectOne(namespace + "readDetail", env_id);
 		return envData;
 	}
 
 	public List<Response> readByCategory(String env_category) {
-		List<Response> envDataList = sqlSession.selectList(namespace+"readByCategory", env_category);
+		List<Response> envDataList = sqlSession.selectList(namespace + "readByCategory", env_category);
 		return envDataList;
 	}
 
 	public List<Response> readByType(String env_type) {
-		List<Response> envDataList = sqlSession.selectList(namespace+"readByType", env_type);
+		List<Response> envDataList = sqlSession.selectList(namespace + "readByType", env_type);
 		return envDataList;
 	}
 
 	public int updateEnvData(Request envDto) {
-		int result = sqlSession.update(namespace+"updateEnvData", envDto);
+		int result = sqlSession.update(namespace + "updateEnvData", envDto);
 		return result;
 	}
 
 	public int deleteEnvData(int env_id) {
-		int result = sqlSession.delete(namespace+"deleteEnvData", env_id);
+		int result = sqlSession.delete(namespace + "deleteEnvData", env_id);
 		return result;
 	}
-	
-	
+
 }
--- src/main/java/com/saeromteo/app/service/EnvDataService.java
+++ src/main/java/com/saeromteo/app/service/EnvDataService.java
@@ -11,41 +11,40 @@ import com.saeromteo.app.dto.EnvironmentDataDto.Response;;
 
 @Service
 public class EnvDataService {
-	
+
 	@Autowired
 	EnvDataDao envDao;
-	
+
 	// Create
 	public int createEnvData(Request envDto) {
 		return envDao.createEnvData(envDto);
 	}
-	
+
 	// Read
 	public List<Response> readAll() {
 		return envDao.readAll();
 	}
-	
+
 	public Response readDetail(int env_id) {
 		return envDao.readDetail(env_id);
 	}
-	
+
 	public List<Response> readByCategory(String env_category) {
 		return envDao.readByCategory(env_category);
 	}
-	
+
 	public List<Response> readByType(String env_type) {
 		return envDao.readByType(env_type);
 	}
-	
+
 	// Update
 	public int updateEnvData(Request envDto) {
 		return envDao.updateEnvData(envDto);
 	}
-	
+
 	// Delete
 	public int deleteEnvData(int env_id) {
 		return envDao.deleteEnvData(env_id);
 	}
-	
-	
+
 }
