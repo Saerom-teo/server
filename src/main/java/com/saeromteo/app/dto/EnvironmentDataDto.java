f7e771ec0d6f86277842192fd69708413cf3fe13
JhyunJ1 <jhyunj1005@gmail.com>
2024-05-28 11:19:15 +0900

    [Rename] : update EnvironmentDataDto

--- src/main/java/com/saeromteo/app/dto/EnvironmentDataDto.java
+++ src/main/java/com/saeromteo/app/dto/EnvironmentDataDto.java
@@ -3,13 +3,27 @@ package com.saeromteo.app.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class EnvironmentDataDto {
-	private int env_id;
-	private String env_title;
-	private String env_content;
-	private String env_data;
-	private String env_category;
-	private String env_type;
+
+	@Getter
+	@Setter
+	static class Response {
+		private int env_id;
+		private String env_title;
+		private String env_content;
+		private String env_data;
+		private String env_category;
+		private String env_type;
+	}
+
+	@Getter
+	@Setter
+	static class Request {
+		private int env_id;
+		private String env_title;
+		private String env_content;
+		private String env_data;
+		private String env_category;
+		private String env_type;
+	}
 }
