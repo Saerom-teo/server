679acec1865613b8226161baf3af7601b6b23a18
imrani-i <tjtpfks@gmail.com>
2024-05-30 14:41:02 +0900

    [Rename] : Save changes in OrderController

---
+++ src/main/java/com/saeromteo/app/controller/order/OrderController.java
@@ -0,0 +1,52 @@
+package com.saeromteo.app.controller.order;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saeromteo.app.model.order.OrderDetailDto.OrderDetailResponse;
+import com.saeromteo.app.model.order.OrderDto.OrderRequest;
+import com.saeromteo.app.model.order.OrderProductDto.OrderProductRequest;
+import com.saeromteo.app.model.order.OrderSuccessDto;
+import com.saeromteo.app.service.order.OrderService;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+	@Autowired
+	OrderService orderService;
+
+	// Create
+	@PostMapping(value = "/createOrderAndProducts", consumes = "application/json", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<String> createOrderWithProducts(@RequestBody OrderSuccessDto orderSuccessDto) {
+        String orderCode = orderService.createOrder(orderSuccessDto.getOrder());
+        if (orderSuccessDto.getProducts() != null && !orderSuccessDto.getProducts().isEmpty()) {
+            orderService.createOrderProducts(orderSuccessDto.getProducts(), orderCode);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderCode);
+    }
+	
+
+	// Read
+	@GetMapping(value = "/readAll", produces = "application/json")
+	public List<OrderDetailResponse> readAll(){
+		List<OrderDetailResponse> orderDetailList = orderService.readAll();
+		return orderDetailList;
+	}
+	
+	@GetMapping(value = "/readByUser", produces = "application/json")
+	public List<OrderDetailResponse> readByUser(@RequestParam int userCode) {
+		List<OrderDetailResponse> orderDetailList = orderService.readByUser(userCode);
+		return orderDetailList;
+	}
+
+}
