f8b31ca56dff4364736e7550cc140c04b412b1f0
DaeHyun2 <hh5576@naver.com>
2024-06-19 09:27:59 +0900

    [Feat] FAQ toggle icon

--- src/main/resources/static/js/faq.js
+++ src/main/resources/static/js/faq.js
@@ -1,11 +1,22 @@
 function toggleAnswer(element) {
     var faqItem = element.closest('.faqItem');
+    if (!faqItem) {
+        console.error('faqItem을 찾을 수 없습니다.');
+        return;
+    }
+    
     var answer = faqItem.querySelector('.faqAnswer');
     var toggleIcon = faqItem.querySelector('.toggleIcon');
 
+    // contextPath가 정의되어 있는지 확인
+    if (typeof contextPath === 'undefined') {
+        console.error('contextPath가 정의되지 않았습니다.');
+        return;
+    }
+
     if (answer.style.display === 'none' || answer.style.display === '') {
         answer.style.display = 'block';
-        toggleIcon.src = contextPath + "/static/img/minusPointer.svg";
+        toggleIcon.src = contextPath + '/static/img/minusPointer.svg';
         toggleIcon.alt = '닫기';
         element.style.backgroundColor = '#F2F8ED';
         element.style.color = '#333232';
