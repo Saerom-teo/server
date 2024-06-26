34a0bacdf44b70d0271c3ca0241525e47ff67175
imrani-i <tjtpfks@gmail.com>
2024-06-26 17:24:15 +0900

    [BugFix] : Sql BugFix

--- src/main/java/com/saeromteo/app/model/order/OrderDto.java
+++ src/main/java/com/saeromteo/app/model/order/OrderDto.java
@@ -17,11 +17,7 @@ public class OrderDto {
 		private Timestamp orderDate;
 		private String orderStatus;
 		private Integer userCode;
-		private String formattedOrderDate;
-		
-		public void setFormattedOrderDate(String formattedOrderDate) {
-        this.formattedOrderDate = formattedOrderDate;
-    }
+	
 	}
 	
 	@Getter
