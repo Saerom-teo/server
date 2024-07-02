df9f665123c4428b44d255cf8115c075b2e0fb54
unknown <isakin.tyrfing@gmail.com>
2024-07-02 17:04:10 +0900

    [Feat] connent with user

--- src/main/java/com/saeromteo/app/service/user/UserService.java
+++ src/main/java/com/saeromteo/app/service/user/UserService.java
@@ -29,16 +29,29 @@ public class UserService implements UserDetailsService {
 	}
 
 	public UserResponse createUserResponseFromUserDTO(UserDTO user) {
-		String[] userAdds = user.getUserAdd().split("/");
-		
+		String userAdd = user.getUserAdd();
+		String[] userAdds = new String[2];
+
+		if (userAdd != null && !userAdd.isEmpty()) {
+			userAdds = userAdd.split("/", 2);
+		}
+
 		UserResponse userResponse = new UserResponse();
-		
+
 		userResponse.setUserId(user.getUserId());
 		userResponse.setUserRealName(user.getUserRealName());
 		userResponse.setUserPhone(user.getUserPhone());
-		userResponse.setUserAddress(userAdds[0]);
-		userResponse.setUserDetailAddress(userAdds[1]);
-		
+		userResponse.setUserAddress(userAdds[0] != null ? userAdds[0] : "");
+		userResponse.setUserDetailAddress(userAdds.length > 1 && userAdds[1] != null ? userAdds[1] : "");
+
 		return userResponse;
+	}
+	
+	public UserDTO readUserByUserId(int userId) {
+		return userDAO.readUserByUserId(userId);
+	}
+	
+	public int updateUser(UserDTO user){
+		return userDAO.updateUser(user);
 	}
 }
